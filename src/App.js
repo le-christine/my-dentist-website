613129fbf50d9647267ba688cbe86f3e0b0833cf
Christine Le <christine.le244@gmail.com>
2019-10-24 13:56:11 -0400

    Tweaking the Browser objects to make routing seamless

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 
 import {
+  HashRouter,
   BrowserRouter as Router,
+  Switch,
   Route,
   Link
 } from 'react-router-dom';
@@ -11,21 +13,26 @@ import {
 import Home from './Home'
 import Procedures from './Procedures';
 import Contact from './Contact';
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router basename={process.env.PUBLIC_URL}>
+        <Router history={history} basename={process.env.PUBLIC_URL}>
         <nav className="nav-bar">
          <Link to="/">Home</Link>{' '}
          <Link to="/procedures">Procedures</Link>{' '}
          <Link to="/contact">Contact</Link>{' '}
         </nav>
          <div>
+          <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/procedures" component={Procedures} />
           <Route path="/contact" component={Contact} />
+          </Switch>
         </div>
         </Router>
       </div>
